498af7fc21fd4a4eeb0995dac90372d4e2bd6373
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-13 21:28:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,8 @@ class Scraper
 
   def self.scrape_index_page(index_url)
 
-    doc = Nokogiri::HTML(open("https://www.cnet.com/topics/headphones/best-headphones/earbuds/")).css("div.bestListings")
+    doc = Nokogiri::HTML(open("https://www.cnet.com/topics/headphones/best-headphones/earbuds/"))
+    array = doc.css("div.bestListings")
 
 binding.pry
     students = doc.collect do |s|
