eb24f47c2a348e5d62dc370e6e2510e50a8e6452
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-13 22:55:17 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,20 +4,35 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
+  def self.scrape_in_ear
+        doc = Nokogiri::HTML(open("https://www.cnet.com/topics/headphones/best-headphones/earbuds/")).css("#rbContent div.bestListing ul li div.itemWrap")
 
-    doc = Nokogiri::HTML(open("https://www.cnet.com/topics/headphones/best-headphones/earbuds/")).css("#rbContent div.bestListing ul li div.itemWrap")
-binding.pry
-    students = doc.collect do |s|
-      s.css(".student-card").collect do |s|
-        {:name => s.css("h4").text,
-         :location => s.css("p").text,
-         :profile_url => s.css("a").attr("href").value
-       }
+        in_ear_array =[]
+
+        doc.each do |headphone|
+          h_name = headphone.css("h5").text
+          h_price = headphone.css(".price")
+          h_url = "https://www.cnet.com#{doc.css(".review").attribute("href").value}"
+          h_rating = headphone[1].css(".subrating").attribute("aria-lable").value
+
+          in_ear_array << {name: h_name, price: h_price, url: h_url, rating: h_rating}
+        end
+        in_ear_array
       end
-    end
-    students.flatten
-  end
+  # def self.scrape_index_page(index_url)
+  #
+  #   doc = Nokogiri::HTML(open(index_url)).css(".roster-cards-container")
+  #
+  #   students = doc.collect do |s|
+  #     s.css(".student-card").collect do |s|
+  #       {:name => s.css("h4").text,
+  #        :location => s.css("p").text,
+  #        :profile_url => s.css("a").attr("href").value
+  #      }
+  #     end
+  #   end
+  #   students.flatten
+  # end
 
   def self.scrape_profile_page(profile_url)
 
